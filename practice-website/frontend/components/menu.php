ed4c25374aac0cadf95a2db35731613a1e3e57d3
Csar9000 <max28k@gmail.com>
2023-12-27 17:21:11 +0800

    осваиваю grid и bootstrap

---
+++ practice-website/frontend/components/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+<nav class="nav flex-column">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+</nav>
+</body>
+</html>
--- practice-website/index.php
+++ practice-website/index.php
@@ -3,19 +3,33 @@
 <head>
   <meta charset="UTF-8">
   <title>Main Page</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-<?php
-  require '..\practice-website\frontend\components\header.php';
-?>
+
+
+<div class="grid">
+  <div class="box- header"> 
+  <?php require '..\practice-website\frontend\components\header.php';?>    
+  </div>
+
+  <div class="box- main"> 
   <form action="/" method="GET">
     <input type="text" name="name" placeholder="Ваше имя"/>
     <input type="submit" value="Отправить"/>
     <h1>
       <?php echo date("d.m.y");  echo '  Hello World!!!';?>
     </h1>
-    
   </form>
+  </div>
+  <div class="box- menuu"> 
+  <?php require('..\practice-website\frontend\components\menu.php'); ?>
+  </div>
+  <div class="box- footer"> 
+  <?php require('..\practice-website\frontend\components\footer.php'); ?>
+  </div>
+</div>
 </body>
 </html>
 
